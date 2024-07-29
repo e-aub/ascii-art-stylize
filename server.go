9cd7e855799bd0c3f9eb7acf4795ca64e7f4eaf3
e-aub <eaub@proton.me>
2024-07-29 16:52:57 +0100

    stylize

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"ascii-art-web/handlers"
+	"ascii-art-web/utils"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	utils.Banners = [5]string{"standard.txt", "shadow.txt", "thinkertoy.txt", "enigma.txt", "nirvana.txt"}
+	//home page route, method : GET
+	http.HandleFunc("/", handlers.HomeHandler)
+	//ascii-art route, method : POST
+	http.HandleFunc("/ascii-art", handlers.AsciiHandler)
+	//serve static files route
+	http.Handle("/static/", (http.StripPrefix("/static/", http.FileServer(http.Dir("static")))))
+	fmt.Println("starting server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
